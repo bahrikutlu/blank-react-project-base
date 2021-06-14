7820855b327ff3ecdeebcdd5b2452b3c75c6d2ff
Bahri Kutlu <63510023+bahrikutlu@users.noreply.github.com>
2021-06-14 17:24:19 -0500

    empty react project with create react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
+import React from "react";
 import Movies from './components/movies';
-
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import {Route, Redirect, Switch} from "react-router-dom";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 
 function App() {
-  return (
-    <main className='container'>
-        <Movies/>
-    </main>
-  );
+    return (
+        <React.Fragment>
+            <NavBar/>
+            <main className='container'>
+                <Switch>
+                    <Route path="/register" component={RegisterForm} />
+                    <Route path="/login" component={LoginForm}/>
+                    <Route path="/movies/:id" component={MovieForm}/>
+                    <Route path="/movies" component={Movies}/>
+                    <Route path="/customers" component={Customers}/>
+                    <Route path="/rentals" component={Rentals}/>
+                    <Route path="/not-found" component={NotFound}/>
+                    <Redirect from="/" exact to="/movies"/>
+                    <Redirect to="/not-found"/>
+                </Switch>
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;
